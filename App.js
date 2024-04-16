8a18997009ce7b224412c52b45f4b2acd4aa311e
GConscious <amrithgandham@gmail.com>
2024-04-16 12:39:35 -0700

    Add createEventScreen

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import HomeScreen from "./screens/HomeScreen";
 import LandingScreen from "./screens/LandingScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import CreateEventScreen from "./screens/CreateEventScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
+      <Stack.Navigator initialRouteName="CreateEvent">
         <Stack.Screen
           name="Landing"
           component={LandingScreen}
@@ -25,6 +26,11 @@ const App = () => {
         <Stack.Screen
           name="SignUp"
           component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateEvent"
+          component={CreateEventScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
